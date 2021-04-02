20cd41e308e8966c60f7817c638fdef8e696a08b
zerodetail_pf <53034761+zerodetail@users.noreply.github.com>
2021-04-02 13:46:04 +1100

    added images

--- cleanup.py
+++ cleanup.py
@@ -1,9 +1,10 @@
 from pathlib import Path
 import os
+import text_to_image
 import shutil
 import random
 
-path = Path("injected")
+path = Path("Injected")
 shellPath = Path("Webshell")
 cleanPath = Path("Clean")
 
@@ -19,14 +20,40 @@ def move(destination):
                 continue
             if not filename.startswith(".") and filename.endswith(".php"):
                 all_files.append(os.path.join(root, filename))
-            else:
-                print(f"deleting {filename}")
-                os.remove(os.path.join(root, filename))
+            # else:
+            #     print(f"deleting {filename}")
+            #     os.remove(os.path.join(root, filename))
     for filename in all_files:
         print(f"Moving {filename} to {filename+'.txt'}")
         shutil.move(filename, str(destination)+"/"+filename.split("\\")[-1]+".txt")
 
 #move(path)
+
+def image(destination):
+    all_files = []
+    first_loop_pass = True
+    for root, _dirs, files in os.walk(destination):
+        if first_loop_pass:
+            first_loop_pass = False
+            continue
+        for filename in files:
+            if not filename.startswith(".") and filename.endswith(".txt"):
+                all_files.append(os.path.join(root, filename))
+            else:
+                print(f"skipping {filename}")
+    failCount = 0
+    for filename in all_files:
+        try:
+            text_to_image.encode_file(filename, f"{filename}.png", 65536)
+        except Exception as e:
+            print(e)
+            failCount += 1
+        
+    print(f"Fail Count: {failCount}")
+
+image(".")
+image(shellPath)
+image(cleanPath)
 
 def createInjected():
     for x in range(2000):
@@ -58,4 +85,4 @@ def createInjected():
 
 
 
-createInjected()
+#createInjected()
