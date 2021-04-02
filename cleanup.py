8d92195f8a674aa4511c1b5231d7e1be4f6d05c6
zerodetail_pf <53034761+zerodetail@users.noreply.github.com>
2021-04-02 12:17:05 +1100

    added injected webshell category

--- cleanup.py
+++ cleanup.py
@@ -1,8 +1,11 @@
 from pathlib import Path
 import os
 import shutil
+import random
 
-path = Path("Clean")
+path = Path("injected")
+shellPath = Path("Webshell")
+cleanPath = Path("Clean")
 
 def move(destination):
     all_files = []
@@ -23,5 +26,36 @@ def move(destination):
         print(f"Moving {filename} to {filename+'.txt'}")
         shutil.move(filename, str(destination)+"/"+filename.split("\\")[-1]+".txt")
 
-move(path)
+#move(path)
 
+def createInjected():
+    for x in range(2000):
+        try:
+            randWebShell = random.choice(os.listdir(shellPath))
+            randClean = random.choice(os.listdir(cleanPath))
+            
+            shellInjected = False
+            with open(shellPath/randWebShell, "r") as shellFile:
+                injectionChance = sum(1 for _ in shellFile) 
+
+            with open(path/f"injected{x}.txt", "w") as injFile:
+                with open(cleanPath/randClean, "r") as cleanFile:
+                    for line in cleanFile:
+                        injFile.write(line)
+                        inj = random.randint(1, injectionChance)
+                        if inj == injectionChance and shellInjected == False:
+                            with open(shellPath/randWebShell, "r") as shellFile:
+                                for shellLine in shellFile:
+                                    injFile.write(shellLine)
+                            shellInjected = True
+                    
+                    if shellInjected == False:
+                        with open(shellPath/randWebShell, "r") as shellFile:
+                            for shellLine in shellFile:
+                                injFile.write(shellLine)
+        except:
+            continue
+
+
+
+createInjected()
